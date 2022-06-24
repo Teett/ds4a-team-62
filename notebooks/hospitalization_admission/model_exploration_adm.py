1d84c14655b1373cd93807b85ab91dc26de64d66
Daniel Chavarría <46414320+Teett@users.noreply.github.com>
2022-06-23 23:41:56 -0500

    Add conf_mat prob threshold experimentation

--- notebooks/hospitalization_admission/model_exploration_adm.py
+++ notebooks/hospitalization_admission/model_exploration_adm.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import pickle
+import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
@@ -9,7 +10,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
 from xgboost import XGBClassifier
 from sklearn import model_selection
-from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay, recall_score, make_scorer, f1_score, balanced_accuracy_score
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay, recall_score, make_scorer, f1_score, balanced_accuracy_score, accuracy_score, roc_auc_score
 
 # %% read data 
 X_train = pd.read_pickle('../../data/processed/admission/X_train_adm.pickle')
@@ -88,3 +89,65 @@ models[0].groupby('model')['test_roc_auc', 'test_sensitivity', 'test_specificity
 models[0].groupby('model')['fit_time'].mean()
 with open("../../models/admission/many_models_adm_1.pickle", "wb") as fp:   #Pickling
     pickle.dump(models, fp)
+
+# %% try Lasso on it's own and verify the coefficients to check if a variable was eliminated
+target_names = ["Not Hospitalized" , "Hospitalized"]
+threshold_list = [0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,.7,.75,.8,.85,.9,.95,.99]
+
+lasso = LogisticRegression(penalty='elasticnet', solver='saga', l1_ratio=1, max_iter=1500)
+lasso_clf = lasso.fit(X_train, y_train)
+
+# %%
+pred_proba_df = pd.DataFrame(lasso_clf.predict_proba(X_test))
+
+
+for i in threshold_list:
+    print ('\n******** For i = {} ******'.format(i))
+    y_test_pred = pred_proba_df.applymap(lambda x: 1 if x>i else 0)
+    test_sensitivity = recall_score(y_test.to_numpy().reshape(y_test.to_numpy().size,1),
+                                           y_test_pred.iloc[:,1].to_numpy().reshape(y_test_pred.iloc[:,1].to_numpy().size,1))
+    print('Our testing sensitivity is {}'.format(test_sensitivity))
+
+    print(confusion_matrix(y_test.to_numpy().reshape(y_test.to_numpy().size,1),
+                           y_test_pred.iloc[:,1].to_numpy().reshape(y_test_pred.iloc[:,1].to_numpy().size,1)))
+
+lasso_coefs = pd.concat([pd.DataFrame(X_train.columns),pd.DataFrame(np.transpose(lasso_clf.coef_))], axis = 1)
+
+# %% try Ridge on it's own and verify the coefficients to check if a variable was eliminated
+ridge = LogisticRegression(penalty='elasticnet', solver='saga', l1_ratio=0, max_iter=1500)
+ridge_clf = ridge.fit(X_train, y_train)
+
+# %%
+pred_proba_df = pd.DataFrame(ridge_clf.predict_proba(X_test))
+
+for i in threshold_list:
+    print ('\n******** For i = {} ******'.format(i))
+    y_test_pred = pred_proba_df.applymap(lambda x: 1 if x>i else 0)
+    test_sensitivity = recall_score(y_test.to_numpy().reshape(y_test.to_numpy().size,1),
+                                           y_test_pred.iloc[:,1].to_numpy().reshape(y_test_pred.iloc[:,1].to_numpy().size,1))
+    print('Our testing sensitivity is {}'.format(test_sensitivity))
+
+    print(confusion_matrix(y_test.to_numpy().reshape(y_test.to_numpy().size,1),
+                           y_test_pred.iloc[:,1].to_numpy().reshape(y_test_pred.iloc[:,1].to_numpy().size,1)))
+
+ridge_coefs = pd.concat([pd.DataFrame(X_train.columns),pd.DataFrame(np.transpose(ridge_clf.coef_))], axis = 1)
+ridge_coefs
+
+# %% try Elastic-net on it's own and verify the coefficients to check if a variable was eliminated
+elastic_net = LogisticRegression(penalty='elasticnet', solver='saga', l1_ratio=0.5, max_iter=1500)
+elastic_net_clf = elastic_net.fit(X_train, y_train)
+
+pred_proba_df = pd.DataFrame(elastic_net_clf.predict_proba(X_test))
+
+for i in threshold_list:
+    print ('\n******** For i = {} ******'.format(i))
+    y_test_pred = pred_proba_df.applymap(lambda x: 1 if x>i else 0)
+    test_sensitivity = recall_score(y_test.to_numpy().reshape(y_test.to_numpy().size,1),
+                                           y_test_pred.iloc[:,1].to_numpy().reshape(y_test_pred.iloc[:,1].to_numpy().size,1))
+    print('Our testing sensitivity is {}'.format(test_sensitivity))
+
+    print(confusion_matrix(y_test.to_numpy().reshape(y_test.to_numpy().size,1),
+                           y_test_pred.iloc[:,1].to_numpy().reshape(y_test_pred.iloc[:,1].to_numpy().size,1)))
+
+elastic_net_coefs = pd.concat([pd.DataFrame(X_train.columns),pd.DataFrame(np.transpose(elastic_net_clf.coef_))], axis = 1)
+elastic_net_coefs
